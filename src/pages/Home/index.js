a365c0717820394be4caea7fc08d570abf03a37e
oliveirajulio <juliocesargomes33@gmail.com>
2024-12-27 13:22:53 -0300

    first commit

---
+++ src/pages/Home/index.js
@@ -0,0 +1,12 @@
+import "./index.css"
+
+function Home() {
+    return (
+        <div className="container">
+            <div className="header"></div>
+            <div className="sidebar"></div>
+        </div>
+    )
+}
+
+export default Home;
---
+++ src/router.js
@@ -0,0 +1,17 @@
+import Home from "./pages/Home";
+
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function Router() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>    
+        </BrowserRouter>
+
+    )
+}
+
+export default Router;

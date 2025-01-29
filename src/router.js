0b822ef8f9a396132407a400fae0a79070425989
oliveirajulio <juliocesargomes33@gmail.com>
2025-01-28 22:26:58 -0300

    merge login

--- src/router.js
+++ src/router.js
@@ -3,14 +3,16 @@ import Register from "./pages/Register"
 import Login from "./pages/Login"
 
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+const isAuthenticated = localStorage.getItem('token'); // Verifica se o usuário está autenticado
 
 function Router() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/signup" element={<Register />}/>
-                <Route path="/" element={<Home />} />
+                <Route path="/" element={isAuthenticated ? <Home /> : <Navigate to="/login" />} />
                 <Route path="/login" element={<Login />} />
             </Routes>    
         </BrowserRouter>

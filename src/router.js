293680863fd919d07dfb3344ba397e15bc17ff33
oliveirajulio <juliocesargomes33@gmail.com>
2025-05-16 16:55:15 -0300

    att 2

--- src/router.js
+++ src/router.js
@@ -7,11 +7,13 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 function Router() {
 
+    const isAuthenticated = localStorage.getItem('token');
+
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/signup" element={<Register />}/>
-                <Route path="/" element= {<Home /> }/>
+                <Route path="/" element={isAuthenticated ? <Home /> : <Navigate to="/login" />} />
                 <Route path="/login" element={<Login />} />
             </Routes>    
         </BrowserRouter>

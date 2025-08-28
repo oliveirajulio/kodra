47ab0b3694f3c54f24db231cf29fd7c9be0896d4
oliveirajulio <juliocesargomes33@gmail.com>
2025-08-28 08:00:49 -0300

    plans page

--- src/pages/Plans/index.js
+++ src/pages/Plans/index.js
@@ -6,9 +6,32 @@ function Plans () {
         <div className="list-plans">
             <nav className="nav-plans">
                 <ul>
-                    <button className="plan-free"></button>
-                    <button className="plan-premium"></button>
-                    <button className="plan-pro"></button>
+                    <li className="plan-free">
+                        <div className="intro-plan">
+                            <span className="name-plan-free">Free</span>
+                            <span className="price-plan-free">R$</span>
+                            <hr className="line-horiz"/>
+                        </div>
+                    </li>
+                    <li className="plan-premium">
+                        <div className="intro-plan">
+                            <span className="name-plan-basic">Basic</span>
+                            <span className="price-plan-basic">R$</span>
+                            <hr className="line-horiz"/>
+
+                        </div>
+
+                    </li>
+                    <li className="plan-pro">
+                        <div className="intro-plan">
+                            <span className="name-plan-premium">Premium</span>
+                            <span className="price-plan-premium">R$</span>
+                            <hr className="line-horiz"/>
+
+
+                        </div>
+
+                    </li>
                 </ul>
             </nav>
         </div>
